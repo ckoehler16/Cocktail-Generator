d434a4ae835f047be0b89f996653915c1ac08ea5
Koehler <ckoehler16@gmail.com>
2022-04-14 00:39:44 -0400

    saved seached ingredients to localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,13 @@ var button = document.querySelector(".button");
 var ingredientInputForm = document.querySelector("#ingredientInput-form");
 var apiKey = "717fd2ea79msh05cd2674261c989p10bbf5jsnc8072009ac5c";
 
+// variables for local storage
+var storedListEl = document.querySelector(".storedList");
+var storageBtnEl = document.querySelector(".storageBtn");
+var mealContainerEl = document.querySelector(".matchCocktailside");
+
+var ingredientIdCounter = 0
+var ingredients = [];
 //   var ingredientInput = ingredientInputEl.value.trim();
 //   if (ingredientInput) {
 //     getRecipe(ingredientInput);
@@ -24,6 +31,7 @@ button.addEventListener("click", function () {
       "X-RapidAPI-Host": "tasty.p.rapidapi.com",
       "X-RapidAPI-Key": "717fd2ea79msh05cd2674261c989p10bbf5jsnc8072009ac5c",
     },
+
   };
   fetch(
     // Fetch recipes from endpoint
@@ -31,7 +39,7 @@ button.addEventListener("click", function () {
     // Use Size param - amount of recipes
     // Use Tags param - recipe tag(s)
     "https://tasty.p.rapidapi.com/recipes/list?from=0&size=10&tags=" +
-      ingredientInput,
+    ingredientInput,
     options
   ).then(function (response) {
     if (response.ok) {
@@ -54,12 +62,17 @@ button.addEventListener("click", function () {
         });
         var recipeLink = document.getElementById("recipeContainer");
         recipeLink.innerHTML = innerHtml;
+
+
       });
     } else {
       alert("Error Please enter a different ingredient");
     }
-  });
 
+  });
+  ingredients.push(ingredientInput);
+  saveIngredient();
+  ingredientIdCounter++;
   // button.addEventListener("click", function (event) {
   //   var ingredientInput = ingredientInputEl.value.trim();
   //   fetch(
@@ -75,4 +88,11 @@ button.addEventListener("click", function () {
   //     })
   //   );
 });
+
+// save ingredients to local storage
+var saveIngredient = function () {
+  localStorage.setItem("ingredients", JSON.stringify(ingredients));
+};
+
 // getRecipe();
+
